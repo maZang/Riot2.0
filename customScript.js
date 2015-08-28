91c0a662cf47d2d1738ac7d9cd4aa1775c672e2f
shpach <shandilya.pachgade@gmail.com>
2015-08-28 11:57:49 -0500

    fixing angular later tonight

--- customScript.js
+++ customScript.js
@@ -1,12 +1,21 @@
 (function(){
 
-var app = angular.module("", []);
+var app = angular.module("champPuller", []);
 
-var MainController = function($scope){
+var PictureController = function($scope, $http){
+
+
+
+var ifError = function(noChamp){
+	$scope.error = "Champion not found!";
+}
 
 };
 
+$scope.search = function{
+	$http.get("http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/" + $scope.champName + ".png").then(avatarReady,ifError);
+}
 
-app.controller("MainController" , MainController);
+app.controller("PictureController" , PictureController);
 
 }());
