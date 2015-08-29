80d668c48a0bf4b1c2ceb57b6763e515a61dd79a
shpach <shandilya.pachgade@gmail.com>
2015-08-28 20:12:23 -0500

    help me get simple data binding angular expression to work

--- customScript.js
+++ customScript.js
@@ -1,21 +1,28 @@
-(function(){
+// (function(){
 
-var app = angular.module("champPuller", []);
+// var app = angular.module("champPuller", []);
 
-var PictureController = function($scope, $http){
+// var PictureController = function($scope, $http){
 
+// var pictureReady = function(response){
+// 	$scope.champion = response.data
+// }
 
+// var ifError = function(noChamp){
+// 	$scope.error = "Champion not found!";
+// }
 
-var ifError = function(noChamp){
-	$scope.error = "Champion not found!";
-}
+// 	$scope.message = "Test"
+// };
 
+// $scope.search = function{
+// 	$http.get("http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/" + $scope.champName + ".png").then(avatarReady,ifError);
+// }
+
+// app.controller("PictureController" , PictureController);
+
+// }());
+
+var MainController = function($scope){
+	$scope.message = "Test";
 };
-
-$scope.search = function{
-	$http.get("http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/" + $scope.champName + ".png").then(avatarReady,ifError);
-}
-
-app.controller("PictureController" , PictureController);
-
-}());
