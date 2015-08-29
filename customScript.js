b0df3cc772a8cf94bd4a614a0e5ec70d24b775fd
shpach <shandilya.pachgade@gmail.com>
2015-08-28 23:18:05 -0500

    help me

--- customScript.js
+++ customScript.js
@@ -11,14 +11,13 @@ var pictureReady = function(response){
 var ifError = function(noChamp){
 	$scope.errorMsg = "Champion not found!";
 }
-
-	$scope.message = "Test"
+	$scope.champName = "Pony"
 };
 
 $scope.search = function{
 	$http.get("http://ddragon.leagueoflegends.com/cdn/5.16.1/img/champion/Chogath.png").then(avatarReady,ifError);
 }
 
-app.controller("PictureController" , PictureController);
+app.controller("PictureController" , ["$scope", "$http", PictureController]);
 
 }());
