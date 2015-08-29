896801cfde7565785de9f47a3c1079d8e59bf97b
shpach <shandilya.pachgade@gmail.com>
2015-08-28 22:58:29 -0500

    partial commit

--- customScript.js
+++ customScript.js
@@ -1,27 +1,24 @@
-// (function(){
+(function(){
 
 var app = angular.module("champPuller", []);
 
-// var PictureController = function($scope, $http){
+var PictureController = function($scope, $http){
 
-// var pictureReady = function(response){
-// 	$scope.champion = response.data
-// }
+var pictureReady = function(response){
+	$scope.champion = response.data
+}
 
-// var ifError = function(noChamp){
-// 	$scope.error = "Champion not found!";
-// }
+var ifError = function(noChamp){
+	$scope.errorMsg = "Champion not found!";
+}
 
-// 	$scope.message = "Test"
-// };
+	$scope.message = "Test"
+};
 
-// $scope.search = function{
-// 	$http.get("http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/" + $scope.champName + ".png").then(avatarReady,ifError);
-// }
+$scope.search = function{
+	$http.get("http://ddragon.leagueoflegends.com/cdn/5.16.1/img/champion/Chogath.png").then(avatarReady,ifError);
+}
 
-// app.controller("PictureController" , PictureController);
+app.controller("PictureController" , PictureController);
 
-// }());
-app.controller("MainController", ["$scope", function($scope){
-	$scope.message = "Test";
-}]);
+}());
