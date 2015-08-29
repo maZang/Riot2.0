ce4b4b507038601af624d1c2c250ce3358235d8c
shpach <shandilya.pachgade@gmail.com>
2015-08-28 23:21:13 -0500

    Pony works now

--- customScript.js
+++ customScript.js
@@ -14,10 +14,6 @@ var ifError = function(noChamp){
 	$scope.champName = "Pony"
 };
 
-$scope.search = function{
-	$http.get("http://ddragon.leagueoflegends.com/cdn/5.16.1/img/champion/Chogath.png").then(avatarReady,ifError);
-}
-
 app.controller("PictureController" , ["$scope", "$http", PictureController]);
 
 }());
