e0a9d4f766773ee6ab035353b1f991d76e950a98
Matthew Zang <matthewzang1@dhcp-mcfad-1759.eduroam.cornell.edu>
2015-08-31 22:14:33 -0400

    k

--- champ_dict_script.py
+++ champ_dict_script.py
@@ -6,6 +6,8 @@ import math
 
 with open('champ_dict.json', 'r') as df:
 	CHAMP_TO_MATRIX = json.load(df)
+with open('spell_dict.json', 'r') as df:
+	spell_dict = json.load(df)
 roles = {0: "Overall", 1: "Physical Bruiser", 2: "Magical Bruiser", 3: "Assassin", 4: "Jungler", 5: "Mage", 6: "Marksman", 7: "Support", 8: "Tank"}
 def main():
 	data = _normalize_data(pd.read_csv("./data1.csv").values)
@@ -39,9 +41,7 @@ def main():
 			dict_data['role'] = roles[idx]
 			play_data_item['label'] = roles[idx]
 			if idx == 0:
-				spell_1 = data_table[row, len(Consts.STAT_TO_MATRIX) + Consts.VARIABLE_TO_MATRIX['spell1id']]
-				spell_2 = data_table[row, len(Consts.STAT_TO_MATRIX) + Consts.VARIABLE_TO_MATRIX['spell2id']]
-				spell_1, spell_2 = get_summoner_spells(spell_1, spell_2)
+				spell_1, spell_2 = get_summoner_spells(champ_name, spell_dict)
 				dict_data['spell1'] = spell_1
 				dict_data['spell2'] = spell_2
 				dict_data['health_built'] = data_table[row, Consts.STAT_TO_MATRIX['FlatHPPoolMod']]
@@ -58,40 +58,24 @@ def main():
 	with open('play_data.json', 'w') as fp:
 		json.dump(play_data, fp)
 
-def get_summoner_spells(spell1, spell2):
-	maths = spell1 ** 2 - 4 * spell2
-	if maths < 0:
-		return "SummonerFlash", "SummonerDot"
-	x = 1/2 * (spell1 - math.sqrt(spell1 ** 2  - 4 * spell2))
-	y = 1/2 * (math.sqrt(spell1 ** 2- 4 * spell2)  + spell1)
-	x = rounding(x)
-	y = rounding(y)
+def get_summoner_spells(champ_name, spell_dict):
+	champspell_dict = spell_dict[champ_name]
+	if len(champspell_dict) == 0:
+		return "SummonerMana", "SummonerMana"
+	x = maxidx(champspell_dict)
+	del champspell_dict[x]
+	y = maxidx(champspell_dict)
 	options = {1: "SummonerBoost", 12: "SummonerTeleport", 14: "SummonerDot", 6: "SummonerHaste", 7: "SummonerHeal", 11: "SummonerSmite", 3: "SummonerExhaust", 13: "SummonerMana", 2: "SummonerClairvoyance", 21: "SummonerBarrier", 4: "SummonerFlash"}
-	return options[x], options[y]
+	return options[int(x)], options[int(y)]
 
-def rounding(num):
-	if num > 0 and num < 1.5:
-		return 1
-	if num < 2.5 and num >= 1.5:
-		return 2
-	if num < 3.5 and num >= 2.5:
-		return 3
-	if num < 5 and num >= 3.5:
-		return 4
-	if num >= 5 and num < 6.5:
-		return 6
-	if num >= 6.5 and num < 9:
-		return 7
-	if num >= 9 and num < 11.5:
-		return 11
-	if num >= 11.5 and num < 12.5:
-		return 12
-	if num >= 12.5 and num < 13.5:
-		return 13
-	if num >= 13.5 and num < 17.5:
-		return 14
-	else:
-		return 21
+def maxidx(dicts):
+	dict_values = []
+	for k, v in dicts.items():
+		dict_values.append(v)
+	max_num = max(dict_values)
+	for k, v in dicts.items():
+		if v == max_num:
+			return k 
 
 def _normalize_data(data):
 	num_games = data[:, Consts.BLACK_MARKET_FEATURES - 1]
